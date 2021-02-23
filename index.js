44bc630e669e2c6d5122cec273688c38c13cf139
Samuel Wilson <samwil56@gmail.com>
2021-02-23 03:26:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
 
-  if (cardNumber=='') {return "Sorry, we don't have a credit card on file for you."}
+  if (cardNumber== undefined ) {return "Sorry, we don't have a credit card on file for you."}
   else {
     var tot = total()
     return `Your total cost is $${tot}, which will be charged to the card ` + cardNumber + '.'
