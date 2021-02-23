0d5e6d0c283926dacc1d1e4a2ee2edbdfe13d1c5
Samuel Wilson <samwil56@gmail.com>
2021-02-23 02:42:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
   var ite = item
   var rice = Math.floor(Math.random()*100)+1
-  var newarr = {itemName:`${item}`, price: rice}
+  var newarr = {itemName:`${ite}`, price: rice}
   cart.push(newarr)
   return `${item} has been added to your cart.`
 }
