99e47be1d3ab0acf8db27357dde7843c6098be76
Samuel Wilson <samwil56@gmail.com>
2021-02-23 03:22:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function removeFromCart(item) {
     items.push(ite)
   }
   var n = items.includes(item)
-  if (n=='False') {return 'That item is not in your cart.'}
-  if (n=='True') {
+  if (n=='false') {return 'That item is not in your cart.'}
+  if (n=='true') {
     var ind = items.indexOf(item)
     cart.splice(ind,1)
     return cart
