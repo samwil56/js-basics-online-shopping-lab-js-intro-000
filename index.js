7cfb6e9ed6af8ad1105bc6a1c9b4665d1d363071
Samuel Wilson <samwil56@gmail.com>
2021-02-23 03:28:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,7 +77,8 @@ function placeOrder(cardNumber) {
   if (cardNumber== undefined ) {return "Sorry, we don't have a credit card on file for you."}
   else {
     var tot = total()
-    return `Your total cost is $${tot}, which will be charged to the card ` + cardNumber + '.'
     cart = []
+    return `Your total cost is $${tot}, which will be charged to the card ` + cardNumber + '.'
+
   }
 }
